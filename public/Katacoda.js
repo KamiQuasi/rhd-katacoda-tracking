d57dbf3de5bcc2293227374f31e00f56ff7f5d47
Legend Dary <ldary@redhat.com>
2019-09-17 08:55:15 -0400

    Initial Commit

---
+++ public/Katacoda.js
@@ -0,0 +1,92 @@
+"use strict";
+class Katacoda {
+    constructor() {
+        // this.katacoda = firebase.firestore().collection('katacoda');
+        // this.users = firebase.firestore().collection('users');
+        let dt = new Date();
+        this.metrics = firebase.firestore().collection(dt.toISOString().substr(0, 10));
+        this.url = new URL(location.href);
+        this.course = typeof this.url.pathname !== 'undefined' && this.url.pathname.length > 1 ? this.url.pathname.replace(/\//g, '--').substr(1) : 'some--course--';
+        this.loc = (this.url.searchParams.get('loc') || 'site');
+        this.listen = this.listen.bind(this);
+    }
+    listen() {
+        let metrics = this.metrics;
+        let course = this.course;
+        let loc = this.loc;
+        let locObj = {};
+        let id = (new Date()).getTime().toString();
+        // Count a new user; reloads and delays in starting can affect the end results
+        metrics.doc('totals').set({
+            users: firebase.firestore.FieldValue.increment(1)
+        }, { merge: true });
+        locObj[loc] = {
+            users: firebase.firestore.FieldValue.increment(1)
+        };
+        metrics.doc('locations').set(locObj, { merge: true });
+        // metrics.doc('courses').set(courseObj, {merge: true});
+        window.addEventListener('message', function (e) {
+            let d = e.data;
+            if (e && e.origin && e.origin.indexOf('katacoda.com') >= 0) {
+                //let scenario = katacoda.doc(course+d.scenario.replace(/\//g,'--'));
+                let labels = {}; // scenario.collection('actions');
+                let action = d.action; //actions.doc(d.action);
+                let label = d.label ? d.label : action;
+                let coursename = course + d.scenario.replace(/\//g, '--');
+                let courseObj = {};
+                courseObj[coursename] = { labels: {} };
+                courseObj[coursename].labels[label] = {
+                    total: firebase.firestore.FieldValue.increment(1),
+                    time: firebase.firestore.FieldValue.arrayUnion(e.timeStamp)
+                };
+                // count the total number and timing of starts, steps, last_step, and finish for a course
+                metrics.doc('courses').set(courseObj, { merge: true });
+                //   .collection(coursename).doc(label).set({
+                //     total: firebase.firestore.FieldValue.increment(1),
+                //     time: firebase.firestore.FieldValue.arrayUnion(e.timeStamp)
+                //     }, {merge: true});
+                if (d.action === 'finish') {
+                    // Count the total number complete
+                    metrics.doc('totals').set({
+                        complete: firebase.firestore.FieldValue.increment(1)
+                    }, { merge: true });
+                    courseObj[coursename].complete = firebase.firestore.FieldValue.increment(1);
+                    metrics.doc('courses').set(courseObj, { merge: true });
+                    // Count total number complete for location/workstation
+                    locObj[loc] = { totals: { complete: firebase.firestore.FieldValue.increment(1) } };
+                    metrics.doc('locations').set(locObj, { merge: true });
+                }
+            }
+        }, false);
+    }
+}
+/*
+metrics
+    totals
+        users
+    courses
+        {coursename}
+            {step}
+                total: int
+                time: timestamp
+    locations
+        {location}
+            totals
+                complete: int
+*/
+document.addEventListener('DOMContentLoaded', function () {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    firebase.initializeApp({
+        "apiKey": "AIzaSyA-xSsRmfOSyUrlj1S5HphBMKXo7o7ihPM",
+        "appId": "1:984574064549:web:912913e4664ef5538b1a4c",
+        "databaseURL": "https://rhd-katacoda-tracking.firebaseio.com",
+        "storageBucket": "rhd-katacoda-tracking.appspot.com",
+        "authDomain": "rhd-katacoda-tracking.firebaseapp.com",
+        "messagingSenderId": "984574064549",
+        "projectId": "rhd-katacoda-tracking"
+    });
+    let katacoda = new Katacoda();
+    katacoda.listen();
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+});
---
+++ public/KatacodaData.js
@@ -0,0 +1,104 @@
+"use strict";
+class KatacodaData {
+    constructor() {
+        this._courses = {};
+        firebase.firestore().collection('2019-09-16').get().then(querySnapshot => {
+            querySnapshot.forEach(doc => {
+                this[doc.id] = doc.data();
+            });
+        });
+    }
+    get totals() {
+        return this._totals;
+    }
+    set totals(docRef) {
+        this._totals = docRef;
+        let userEle = document.querySelector('#total-users');
+        let completionEle = document.querySelector('#total-complete');
+        if (userEle) {
+            userEle.innerHTML = this._totals.users ? this._totals.users.toString() : '--';
+        }
+        if (completionEle) {
+            completionEle.innerHTML = this._totals.complete ? this._totals.complete.toString() : '--';
+        }
+        //console.log('Totals', this._totals);
+    }
+    get courses() {
+        return this._courses;
+    }
+    set courses(docRef) {
+        this._courses = docRef;
+        let courseEle = document.querySelector('#total-courses');
+        if (courseEle) {
+            courseEle.innerHTML = Object.keys(this._courses).length.toString();
+        }
+        let courseList = document.querySelector('#courses');
+        for (let [key, value] of Object.entries(this._courses)) {
+            let course = new KatacodaCourse(key, value);
+            let ele = document.createElement('li');
+            ele.innerHTML = `${course}`; //`${course.name} - ${course.complete ? `${course.complete} Completed |`: ''} | ${course.labels}`;
+            if (courseList) {
+                courseList.append(ele);
+            }
+        }
+        //console.log('Courses', this._courses);
+    }
+    get locations() {
+        return this._locations;
+    }
+    set locations(docRef) {
+        this._locations = docRef;
+        let locationList = document.querySelector('#locations');
+        for (let [key, val] of Object.entries(this._locations)) {
+            let ele = document.createElement('li');
+            ele.innerHTML = `<strong>${key}</strong> - ${val.users} <em>Users</em> | ${val.totals && val.totals.complete ? val.totals.complete : '0'} <em>Course Completions</em>`;
+            if (locationList) {
+                locationList.append(ele);
+            }
+        }
+        //console.log('Locations', this._locations);
+    }
+    report() {
+        console.log('Totals', this.totals);
+        console.log('Courses', this.courses);
+        console.log('Locations', this.locations);
+    }
+}
+class KatacodaCourse {
+    constructor(name, labels) {
+        this.labels = [];
+        this.name = name;
+        for (let [key, value] of Object.entries(labels)) {
+            this.labels.push(new KatacodaLabel(key, value));
+        }
+    }
+    toString() {
+        return `${this.name}
+        <ul>
+            ${this.labels.reduce((prev, curr) => {
+            return `${prev}
+        <li>${curr}</li>`;
+        })}
+        </ul>`;
+    }
+}
+class KatacodaLabel {
+    constructor(name, data) {
+        this.name = name;
+        this.time = data.time;
+        this.total = data.total;
+    }
+    toString() {
+        let avgTime = this.time ? this.time.reduce((prev, curr) => {
+            return prev + curr;
+        }) / this.time.length : 0;
+        return `${this.name} - ${avgTime / 3600} Seconds`;
+    }
+}
+document.addEventListener('DOMContentLoaded', function () {
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+    // // The Firebase SDK is initialized and available here!
+    let katacoda = new KatacodaData();
+    // katacoda.report();
+    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+});
